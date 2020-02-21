cab80f4d26cd4f32c0a57b8f70d93c1bb6d0cba9
Buddy Agyin <agyin3@gmail.com>
2020-02-21 11:29:36 -0700

    add validations to tests

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -7,13 +7,20 @@ it('should navigate to the TodoMVC App', () => {
 it('should be able to add a new todo to the list', () => {
 
     cy.get('.new-todo').type('Watch Lakers game{enter}')
+
+    cy.get('label').should('have.text', 'Watch Lakers game')
+
+    cy.get('.toggle').should('not.be.checked')
 })
 
 it('should toggle todo to complete', () => {
 
     cy.get('.toggle').click()
+    cy.get('.toggle').should('be.checked')
+    cy.get('label').should('have.css', 'text-decoration-line', 'line-through')
 })
 
 it('should clear completed todo', () => {
-    cy.contains('Clear completed').click()
+    cy.contains('Clear').click()
+    cy.get('.todo-list').should('not.have.descendants', 'li')
 })
