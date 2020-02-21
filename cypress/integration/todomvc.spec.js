810bb35d3f527faf09652456d5038fe24b1bd6f5
Buddy Agyin <agyin3@gmail.com>
2020-02-21 10:59:22 -0700

    add tests for add todo, toggle, complete, clear

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -4,3 +4,16 @@ it('should navigate to the TodoMVC App', () => {
     cy.visit('http://todomvc-app-for-testing.surge.sh/')
 })
 
+it('should be able to add a new todo to the list', () => {
+
+    cy.get('.new-todo').type('Watch Lakers game{enter}')
+})
+
+it('should toggle todo to complete', () => {
+
+    cy.get('.toggle').click()
+})
+
+it('should clear completed todo', () => {
+    cy.contains('Clear completed').click()
+})
